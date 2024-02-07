44027ce74aaac9f278e342a21fb493576ca23055
Arshath <itizarsa@gmail.com>
2024-02-07 18:58:08 +0530

    refactor(database): remove skill, candidate skills table

--- src/database/schema.js
+++ src/database/schema.js
@@ -1,13 +1,8 @@
-import { int, mysqlEnum, mysqlTable, varchar, serial, primaryKey } from "drizzle-orm/mysql-core"
-import { relations } from "drizzle-orm"
+import { int, mysqlEnum, mysqlTable, varchar, json } from "drizzle-orm/mysql-core"
 
-export const statusEnum = mysqlEnum("status", [
-	"Contacted",
-	"Interview Scheduled",
-	"Offer Extended",
-	"Hired",
-	"Rejected"
-])
+export const status = ["Contacted", "Interview Scheduled", "Offer Extended", "Hired", "Rejected"]
+
+export const statusEnum = mysqlEnum("status", status)
 
 export const candidates = mysqlTable("Candidates", {
 	id: int("id").primaryKey().autoincrement(),
@@ -15,45 +10,6 @@ export const candidates = mysqlTable("Candidates", {
 	email: varchar("email", { length: 256 }).notNull(),
 	phone: varchar("phone", { length: 256 }).notNull(),
 	status: statusEnum.notNull(),
-	expectedSalary: int("expectedSalary")
+	expectedSalary: int("expectedSalary"),
+	skills: json("skills")
 })
-
-export const candidatesRelations = relations(candidates, ({ many }) => ({
-	skills: many(candidatesSkills)
-}))
-
-export const skills = mysqlTable("Skills", {
-	id: int("id").primaryKey().autoincrement(),
-	name: varchar("name", { length: 256 }).notNull(),
-	displayName: varchar("displayName", { length: 256 }).notNull()
-})
-
-export const skillsRelations = relations(skills, ({ many }) => ({
-	candidates: many(candidatesSkills)
-}))
-
-export const candidatesSkills = mysqlTable(
-	"CandidatesSkills",
-	{
-		candidateId: int("candidateId")
-			.notNull()
-			.references(() => candidates.id),
-		skillId: int("skillId")
-			.notNull()
-			.references(() => skills.id)
-	},
-	t => ({
-		pk: primaryKey({ columns: [t.candidateId, t.skillId] })
-	})
-)
-
-export const candidateSkillsRelations = relations(candidatesSkills, ({ one }) => ({
-	candidate: one(candidates, {
-		fields: [candidatesSkills.candidateId],
-		references: [candidates.id]
-	}),
-	skill: one(skills, {
-		fields: [candidatesSkills.skillId],
-		references: [skills.id]
-	})
-}))
