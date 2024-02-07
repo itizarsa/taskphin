32481f0d744f5aebb4bcc7a5299d0f8b5a64991b
Arshath <itizarsa@gmail.com>
2024-02-07 15:26:20 +0530

    refactor(database): remove schema & import db url

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,7 @@
+import { DATABASE_URL } from "../common/config.js"
 import { drizzle } from "drizzle-orm/mysql2"
 import mysql from "mysql2/promise"
 
 export const connection = await mysql.createConnection({ uri: DATABASE_URL })
 
-export const db = drizzle(connection, { schema })
+export const db = drizzle(connection)
