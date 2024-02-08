bc572d311ef8af8f000781bae1da6aa10d74ed5b
Arshath <itizarsa@gmail.com>
2024-02-08 08:27:47 +0530

    refactor(validator): assign validated value

--- src/validator/validator.middleware.js
+++ src/validator/validator.middleware.js
@@ -1,7 +1,7 @@
 import Joi from "joi"
 
 const validate = (schema, type) => (req, res, next) => {
-	const { error } = Joi.compile(schema).validate(req[type], { abortEarly: false })
+	const { error, value } = Joi.compile(schema).validate(req[type], { abortEarly: false })
 
 	if (error) {
 		const errorMessage = error.details.map(details => {
@@ -14,6 +14,8 @@ const validate = (schema, type) => (req, res, next) => {
 
 		return res.status(400).json({ message: "Validation failed", errors: errorMessage })
 	}
+
+	req[type] = value
 
 	return next()
 }
