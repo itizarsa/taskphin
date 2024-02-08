7abfd99d0ebc319fc19abf84b7ac1bba7fdd5575
Arshath <itizarsa@gmail.com>
2024-02-08 08:28:58 +0530

    refactor: remove logger

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import { loggerMiddleware } from "./logger/logger.middleware.js"
 import candidate from "./candidate/candidate.route.js"
-import { logger } from "./logger/logger.utils.js"
 import { PORT } from "./common/config.js"
-import { randomUUID } from "crypto"
-import rTracer from "cls-rtracer"
 import express from "express"
 
 const app = express()
@@ -11,10 +7,6 @@ const app = express()
 app.use(express.json({ limit: "10mb" }))
 
 app.use(express.urlencoded({ extended: true, limit: "10mb" }))
-
-app.use(rTracer.expressMiddleware({ requestIdFactory: () => randomUUID() }))
-
-app.use(loggerMiddleware)
 
 app.use("/candidates", candidate)
 
@@ -29,9 +21,9 @@ app.listen(PORT, () => {
 process.on("unhandledRejection", error => {
 	const { message, stack } = error
 
-	logger.error({ type: "unhandledRejection", message: message || error, stack })
+	console.error({ type: "unhandledRejection", message: message || error, stack })
 })
 
 process.on("uncaughtException", (err, origin) => {
-	logger.error({ type: "uncaughtException", err, origin })
+	console.error({ type: "uncaughtException", err, origin })
 })
